361c76f88de5a0a346e3d795d15c306840f25d1e
secr3thaha <146701719+secr3thaha@users.noreply.github.com>
2023-10-04 20:49:15 +0800

    Add files via upload

---
+++ 1696423597577.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+   int numLines = 100;
+      
+   for (int i = 0; i < numLines; i++) { 
+      printf("I MISS YOU I MISS YOU I MISS YOU ", i) ;
+      getchar() ;
+      //aries/serwen
+   }
+   
+   return 0;
+}
